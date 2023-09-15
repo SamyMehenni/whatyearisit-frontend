89c81681f6d2d2eaeb7d9eec957e03460c73c54d
Samy <mehennisamy@yahoo.fr>
2023-09-15 17:01:38 +0200

    frontend V1

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch("https://whatyearisit-backend-rils.vercel.app/")
+.then(response=> response.json())
+.then(data => {
+    console.log(data)
+    document.querySelector("#year").textContent =  data.year
+
+})
