cdc130a07eb15154b3aba098929debc821c7093f
Samy <mehennisamy@yahoo.fr>
2023-09-15 17:13:43 +0200

    redeploy

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://whatyearisit-backend-rils.vercel.app/")
+fetch("https://whatyearisit-backend-rils.vercel.app/year")
 .then(response=> response.json())
 .then(data => {
     console.log(data)
